9bc9929182ce69defeb63828cee680735c8e8f5f
varun arya <52693350+varun425@users.noreply.github.com>
2022-08-09 12:17:55 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"log"
 
-	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-
+	"github.com/varun425/MiniClubChaincode/erc20"
 	"github.com/varun425/MiniClubChaincode/healthclub"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 func main() {
-	miniclub, err := contractapi.NewChaincode(&healthclub.HealthClub)
+	miniclub, err := contractapi.NewChaincode(&healthclub.HealthClub{}, &erc20.SmartContract{})
 	if err != nil {
 		log.Panicf("Error creating miniclub chaincode: %v", err)
 	}
 
 	if err := miniclub.Start(); err != nil {
-		log.Panicf("Error starting tminiclub chaincode: %v", err)
+		log.Panicf("Error starting miniclub chaincode: %v", err)
 	}
 }
